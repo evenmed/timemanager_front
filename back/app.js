3ea343edf848c2e21b9868a515eb6161282eafd9
Emilio Venegas <emvenmed@gmail.com>
2020-11-16 19:52:03 -0600

    Local mongoose support and no sessions in backend

--- back/app.js
+++ back/app.js
@@ -3,9 +3,6 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const { v4: uuidv4 } = require("uuid");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -16,19 +13,32 @@ const typeDefs = require("./schema/types");
 const isDev = process.env.NODE_ENV === "development";
 
 // Connect to db
-mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(
-    process.env.DB_PASS
-  )}@${process.env.DB_SERVER}/${
-    process.env.DB_NAME
-  }?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    connectTimeoutMS: 4000,
-  }
-);
+
+if (isDev) {
+  mongoose.connect(
+    `mongodb://${process.env.DB_SERVER}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 4000,
+    }
+  );
+} else {
+  mongoose.connect(
+    `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(
+      process.env.DB_PASS
+    )}@${process.env.DB_SERVER}/${
+      process.env.DB_NAME
+    }?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 4000,
+    }
+  );
+}
 
 mongoose.connection.once("open", () => {
   console.log("connected to database");
@@ -49,28 +59,12 @@ app.use(
   })
 );
 
-// Sessions middleware
-// app.use(
-//   session({
-//     genid: () => uuidv4(),
-//     secret: process.env.APP_SECRET,
-//     key: process.env.APP_KEY,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//       maxAge: 3600000 * 24 * 7, // 7 days
-//     },
-//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-//   })
-// );
-
 // Enable cookies
 app.use(cookieParser());
 
 // decode JWT to get userid
 app.use((req, _res, next) => {
   const { token } = req.cookies;
-  console.log(token);
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
     req.userId = userId;
@@ -93,10 +87,7 @@ app.use(async (req, _res, next) => {
 
 // passport config
 app.use(passport.initialize());
-// app.use(passport.session());
 passport.use(User.createStrategy());
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
 
 // Start Apollo Server
 const server = new ApolloServer({
