06cc96a8c5dd69e7563995097eb7110962f884c2
Emilio Venegas <emvenmed@gmail.com>
2020-11-20 09:49:43 -0600

    New prod endpoint

--- front/lib/apolloClient.js
+++ front/lib/apolloClient.js
@@ -4,7 +4,7 @@ const link = createHttpLink({
   uri:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000/graphql"
-      : "https://timemanager-back.herokuapp.com/graphql",
+      : "http://tmback.emiliovenegas.me/graphql",
   credentials: "include",
 });
 
